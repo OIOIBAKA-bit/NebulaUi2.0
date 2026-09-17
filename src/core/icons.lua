a4cf56597851bfaa84392b944a18143e1901e108
OIOIBAKA-bit <oioibakaoioibaka686@gmail.com>
2026-09-16 22:43:35 -0400

    Add icon registry and retrieval function

--- src/core/icons.lua
+++ src/core/icons.lua
@@ -1,1 +1,15 @@
+--!strict
 
+local Icons = {}
+
+local Registry = {
+    home = "rbxassetid://10723434674",
+    settings = "rbxassetid://10734950309",
+    user = "rbxassetid://10734953444",
+}
+
+function Icons.Get(name: string): string
+    return Registry[name] or Registry.home
+end
+
+return Icons
